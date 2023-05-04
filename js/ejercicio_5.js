02882a13a0f90ad1ad29e29811d5ef49cc6abfdc
GermanGutierrez85 <128050377+GermanGutierrez85@users.noreply.github.com>
2023-05-04 15:01:01 -0300

    Add files via upload

---
+++ js/ejercicio_5.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/* Tarea
+ * Objetivos: adquirir herramientas para identificar 
+ * problemas en el código, identificando si se producen
+ * errores de sintaxis o de lógica.
+ * debe descomentar cada ejercicio!
+ */
+ 
+ //-------------------------------------------------------------------
+// 5. Utilizar una sola variable para almacenar texto o numeros, 
+//    e imprimir en consola varias veces para construir la frase:
+//    El numero
+//    7
+//    me trae mas suerte que el numero 
+//    13
+
+
+const textoNumero = "El numero ";
+let numero1 = 7;
+const textoSuerte = " me trae mas suerte que el numero ";
+let numero2 = 13;
+console.log(textoNumero + numero1 + textoSuerte + numero2); //Opcion en una linea.
+
+/*Segunda opcion de impresion. 
+Para esta opcion, debera comentar la constante "textoSuerte"de la
+seccin anteriore*/
+
+/* const textoSuerte = "me trae mas suerte que el numero ";
+console.log(textoNumero);
+console.log(numero1);
+console.log(textoSuerte);
+console.log(numero2); */
